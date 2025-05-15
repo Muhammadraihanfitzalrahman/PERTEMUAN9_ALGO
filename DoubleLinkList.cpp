f09019863fb1c53c1d4b398d3e3b1413e7932796
muhammadraihanfitzalrahman <Ayamtulebo19@gmail.com>
2025-05-15 12:56:37 +0700

    class DoubleLinkList

--- DoubleLinkList.cpp
+++ DoubleLinkList.cpp
@@ -1,6 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
 class mode {
     public:
          int noMhs;
          Node *next;
          Node *prev;
 };
+
+class DoubleLinkList
+{
+    private:
+    Node *START;
+
+    public:
+    DoubleLinkList()
+    {
+        START = NULL;
+    }
+
